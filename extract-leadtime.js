5a4b62424aedd78968b66656877077828e95c8c0
Austen Constable <awconstable@gmail.com>
2019-11-07 12:30:04 +0000

    Fixed proxy requires

--- extract-leadtime.js
+++ extract-leadtime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const fetch = require('fetch-with-proxy');
+require('fetch-with-proxy');
 const dateFormat = require('dateformat');
 const leankitUrl = require('./leankit-url');
 
